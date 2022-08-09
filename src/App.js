47fd24911268e99a470dfbcc8faa96d85c4943af
AntonGreg <antonio.pena@epn.edu.ec>
2022-08-09 07:58:12 -0500

    Fullstack part1 section c : stateful component completed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-const Hello = ({name, age}) => {
+import { useState } from 'react'
 
-  const bornYear = () => new Date().getFullYear() - age;
-  
-  return (
-    <div>
-      <p>Hello {name}, you are {age} years old</p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  );
-}
-const App = (props) => {
-  const {counter} = props
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
+  console.log('rendering...', counter)
+
   return (
     <div>{counter}</div>
   )
 }
 
-
-
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -3,18 +3,4 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-let counter = 1;
-
-const refresh = () => {
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(
-    <React.StrictMode>
-      <App counter={counter} />
-    </React.StrictMode>
-  );
-}
-
-setInterval(() => {
-  refresh()
-  counter += 1
-}, 1000)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
